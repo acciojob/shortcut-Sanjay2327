ecdf67a7f44c3f417ed6099e5b40ede2d3da2405
Sanjay Kushwaha <sanjaykushwaha80676@gmail.com>
2026-07-11 10:10:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function shortcut(s1, s2) {
   // your code here
+	let s1 = "" ;
+	let s2 = "" ;
+	let char1 = s1.charAt(0);
+	let char2 = s2.charAt(0);
+	return char1 + char2 ;
 }
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+alert(shortcut(s1, s2)); 
